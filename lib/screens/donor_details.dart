2229e4b139e29b2f6c7cc57b156b35e5844652e2
hari75 <harigovind7500@gmail.com>
2020-10-31 11:10:54 +0530

    chat

--- lib/screens/donor_details.dart
+++ lib/screens/donor_details.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:aider/screens/donordash.dart';
 
-
 class DonorDetails extends StatefulWidget {
   @override
   _DonorDetailsState createState() => _DonorDetailsState();
 }
 
 class _DonorDetailsState extends State<DonorDetails> {
+  final _description = TextEditingController();
+  bool _validatedescription = false;
+  final _quantity = TextEditingController();
+  bool _validatequantity = false;
+  int _value = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //bottom notched bar
+        bottomNavigationBar: new BottomAppBar(
+          color: Color(0xFF2B2D42),
+          shape: CircularNotchedRectangle(),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.circle,
+                  color: Color(0xFF2B2D42),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          backgroundColor: Color(0xFF2B2D42),
+          child: Icon(Icons.home_outlined),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => Donordash(),
+              ),
+            );
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        //bottom notched bar ends
+        resizeToAvoidBottomInset: false,
         body: Container(
             decoration: BoxDecoration(
                 color: Color(0xFFEDF2F4),
                 image: DecorationImage(
                     image: AssetImage('images/background.jpg'))),
+            width: double.infinity,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -103,19 +142,21 @@ class _DonorDetailsState extends State<DonorDetails> {
                   width: 100.0,
                   height: 50.0,
                   child: OutlineButton(
-                            // borderSide: BorderSide.none,
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                            onPressed: () {
-                              print("Pressed Edit");
-                            },
-                            child: Center(
-                              child: Text("Edit", style: TextStyle(
-                        fontSize: 20.0,
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.bold,
-                      ),)
-                            )),
+                      // borderSide: BorderSide.none,
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(5.0)),
+                      onPressed: () {
+                        print("Pressed Edit");
+                      },
+                      child: Center(
+                          child: Text(
+                        "Edit",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ))),
                 ),
               ],
             )));
